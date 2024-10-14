41cd7beb95f2960706a015221a10b75576443be5
Hector Corbellini <hector.corbellini@gmail.com>
2024-10-14 11:04:27 -0300

    Ajustado a patrón de diseño Singleton, en la clase GameService. Ventajas: - Se crea una sola instancia de GameService para cualquier servlet o componente. - memoria limitada a una única instanciación - La lazyInitialization asegura que GameService se instanciaría con cuidado si hay varios hilos.

--- src/test/java/QuestionServletTest.java
+++ src/test/java/QuestionServletTest.java
@@ -1,5 +1,6 @@
-package com.juego;
-
+import com.juego.GameService;
+import com.juego.Question;
+import com.juego.QuestionServlet;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
